803d43de4d92bb1bee2817e5735dc7a71e09c994
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-07-08 11:27:41 +0000

    feat(nix): add flake.nix for uv dev shell

---
+++ uv/flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkGs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            uv
+          ];
+        };
+      }
+    );
+}
